aa668bcd7e172706919b38e3c88dbe6381870bc2
Sandeep Salla <156847393+sandeep-s369@users.noreply.github.com>
2024-02-15 23:04:23 +0530

    Update EmployeeRepository.java

--- ReactJS-Spring-Boot-CRUD-Full-Stack-App/springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
+++ ReactJS-Spring-Boot-CRUD-Full-Stack-App/springboot-backend/src/main/java/net/javaguides/springboot/repository/EmployeeRepository.java
@@ -1,11 +1,20 @@
 package net.javaguides.springboot.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import org.springframework.data.jpa.repository.Query;
 
 import net.javaguides.springboot.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+  @Query(value = "SELECT * FROM Employee WHERE firstName = :employeeFirstName", nativeQuery = true)
+	Optional<List<Employee>> findEmployeesByFirstName(String employeeFirstName);
+
 
 }
